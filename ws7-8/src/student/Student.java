bcac26fb8d98619e7cbac2b168348b9c4c820059
Wenxuan Liu <wliu122@myseneca.ca>
2019-07-24 07:54:41 -0400

    remove useless imports

--- ws7-8/src/student/Student.java
+++ ws7-8/src/student/Student.java
@@ -1,17 +1,5 @@
 package student;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.function.Consumer;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
-import java.util.stream.Collectors;
-import java.util.Optional;
 
 public class Student {
     private String firstName;
